d86d9ce3f1b10240d8602c14a10d46b01af859c4
fdtroya <fdtroya@espol.edu.ec>
2022-12-10 03:09:31 +0900

    better Ranges

--- tests/protocol2_0/Custom/control/systemID/optimize.py
+++ tests/protocol2_0/Custom/control/systemID/optimize.py
@@ -8,32 +8,41 @@ from scipy.optimize import basinhopping
 from scipy.optimize import shgo
 from scipy.optimize import  dual_annealing
 import matplotlib.pyplot as plt
+import json
+
+def loadJson(file):
+    with open(file) as json_file:
+        data = json.load(json_file)
+        return data
 
 outputDataFr=np.load("dataFrFiltered.npy")#change name 
 outputData=np.load("dataHFiltered.npy")#change name 
-Ra=8.1
-La=0.28*10**-3
+outputDataStable=np.load("stableStateData.npy")
+models=loadJson("models.json")
 
-J=(5.41*353.5)*10**-8
-Kt=2.67857142857
-B=(3*353.5)*10**-7
 r=(22.28/2)*10**-3
-min=10**-6
 
 
 
-bounds=[(8,9.5),(0.26*10**-3,0.32*10**-3),(9*10**-4,7*10**-3),(2.65,2.8),(9*10**-2,7*10**-1)]
-guess=[9.25170038e+00 ,2.74492995e-04, 6.94938581e-03 ,2.68651742e+00,2.89755777e-01]
 
-bounds1=[(9*10**-4,7*10**-3),(2.65,2.8),(9*10**-2,7*10**-1)]
-guess1=[2.16827014e-03, 2.74042159e+00 ,2.95628771e-01]
 
-boundsFr=[(85000,100000),(2500,4500),(0.05,0.07),(0.15,0.3),(0.02,0.17),(0.0000001,0.001)]
-guessFr=[8.79424207e+04, 3.28712928e+03, 6.71277751e-02 ,1.58548656e-01,1.50433937e-01, 6.18313380e-04]
+
+boundsFr=[(100,5000),(5,200),(0.03,0.2),(0.11,0.18),(0.03,0.11),(0.002,0.1)]
+
+guessFr=[3.53576995e+03,1.31802431e+02,1.54433544e-01,1.33292172e-01,1.09904280e-01,0.005]#9.36241349e-02]
+gFr2=[1.17598979e+03,3.75332452e+01,1.53647825e-01,1.42334107e-01,1.09927725e-01,9.36358140e-02]
+boundsSS=[(0.03,0.1),(0.1,0.18),(0.03,0.12),(0.008,0.1)]
+guessSS=[0.0328251,0.14357413,0.10497904,0.03391913]
+
+
 
 endTimeFr=10
 endTime=10
-Ra,La,J,Kt,B=guess
+Ra=models["motor"]["Ra"]
+La=models["motor"]["La"]
+J=models["motor"]["J"]
+Kt=models["motor"]["Kt"]
+B=models["motor"]["B"]
 
 
 experimentalOmegaFrf=interpolate.interp1d(outputDataFr[:,-1], outputDataFr[:,0],fill_value=0,bounds_error=False)
@@ -50,21 +59,7 @@ def signalFr(t):# t in seconds [0 10]
 def signal(t):# t in seconds [0 10]
     return (7*math.sin((math.pi/endTime)*t),0) 
 
-def func(x,plot=False):
-    Ra,La,J,Kt,B=list(x)
-    
-    individual=Model.motorModel(Ra,La,J,Kt,B,initialState=[0,0],inputFunction=signal)
-    f=interpolate.interp1d(individual.t, individual.y[0],fill_value=0,bounds_error=False)
-    ts=np.arange(0,endTime,0.001)
-    experimentalOmega=np.clip(experimentalOmegaf(ts),0,None)
-    individualOmega=f(ts)
-    squareErrorArray=(individualOmega-experimentalOmega)**2
-    if(plot):
-        plt.plot(individual.t,individual.y[0])
-        plt.plot(ts,experimentalOmega)
-        plt.show()
-    squareErrorTot= np.sum(squareErrorArray)
-    return squareErrorTot
+
 def funcFr(x,plot=False):
     interest=10
     sigma0,sigma1,sigma2,Ts,Tc,Vs=list(x)
@@ -76,86 +71,42 @@ def funcFr(x,plot=False):
     individualOmega=f(ts)
     squareErrorArray=(individualOmega-experimentalOmega)**2
     if(plot):
-        plt.plot(individual.t,individual.y[1])
+        plt.plot(individual.t,individual.y[0])
         plt.plot(ts,experimentalOmega)
         plt.show()
     squareErrorTot= np.sum(squareErrorArray)
     return squareErrorTot
 def funcSt(x,plot=False):
     sigma2,Ts,Tc,Vs=x
-    omegas=[]
-    TfrLs=[]
-    for omega in omegas:
-        Tfr=(Tc+(Ts-Tc)*math.e**-(omega/Vs)**2) *sign(omega)+sigma2*omega
-        TfrLs.append(Tfr)
-    TfrArr=np.array(TfrLs)
+    interval=[0,3]
+
+    omegaLs=outputDataStable[:,1]
+    mask1=omegaLs<=interval[-1]
+    mask2=omegaLs>=interval[0]
+    mask=mask1*mask2
+    omegaLs=omegaLs[mask]
+    Torques=outputDataStable[:,0][mask]
+    def Tf(omegas):
+        TfrLs=[]
+        for omega in omegas:
+            Tfr=(Tc+(Ts-Tc)*math.e**-(omega/Vs)**2) *sign(omega)+sigma2*omega
+            TfrLs.append(Tfr)
+        TfrArr=np.array(TfrLs)
+        return TfrArr
+    err=(Tf(omegaLs)-Torques)**2
+    if plot:
+        ws=np.arange(-3,3,0.01)
+        plt.plot(ws,Tf(ws),'.')
+        plt.plot(outputDataStable[:,1],outputDataStable[:,0],'.')
+        plt.show()
+    return np.sum(err)
     
-def evolve():
-    strats='best1exp'
-    maxIter=300
-    popSize=100
-    mutation=(0.5,1.5)
-    CR=0.25
-    disp=True
-    polish=True
-    init='halton'
-    atol=0.0001
-    s=differential_evolution(func,strategy=strats,maxiter=maxIter,popsize=popSize,mutation=mutation,recombination=CR,disp=disp,polish=polish,bounds=bounds,init=init,atol=atol,updating='immediate',x0=guess)
-    print(s.x)
-    print(s.success)
-    print(s.message)
-    print(func(s.x,plot=True))
-def Hopping():
-    maxIter=1000
-    s=basinhopping(func,guess)
-    print(s.x)
-    print(s.success)
-    print(s.message)
-    print(func(s.x,plot=True))
-def SH():
-    maxIter=5
-    s=shgo(func,bounds=bounds,iters=maxIter,options={'disp':True})
-    print(s.x)
-    print(s.success)
-    print(s.message)
-    print(func(s.x,plot=True))  
-def dual():
-    maxIter=1000
-    s=dual_annealing(func,bounds=bounds,maxiter=maxIter,x0=guess)
-    print(s.x)
-    print(s.success)
-    print(s.message)
-    print(func(s.x,plot=True))  
 
-
-def minimization():
-    methods=['Nelder-Mead','Powell','L-BFGS-B','TNC','COBYLA','trust-constr']
-    for method in methods:
-        s=minimize(func,guess1,method=method,bounds=bounds1)
-        print(method)
-        print(s.x)
-        print(s.success)
-        print(s.message)
-        print(func(s.x,True))
-        print("################################")
-
-
-def minimizationFr():
-
-    methods=['Nelder-Mead','Powell','L-BFGS-B','TNC','COBYLA','trust-constr']
-    for method in methods:
-        s=minimize(funcFr,guessFr,method=method,bounds=boundsFr)
-        print(method)
-        print(s.x)
-        print(s.success)
-        print(s.message)
-        print(funcFr(s.x))
-        print("################################")
 
 def evolveFr():
     strats='best1bin'
     maxIter=50
-    popSize=100
+    popSize=50
     mutation=(0.2,1.5)
     CR=0.8
     disp=True
@@ -166,35 +117,49 @@ def evolveFr():
     print(s.x)
     print(s.success)
     print(s.message)
-    print(funcFr(s.x,True))
+    return s.x
+    
+def evolveFrSS():
+    strats='best1bin'
+    maxIter=250
+    popSize=1000
+    mutation=(0.2,1.5)
+    CR=0.8
+    disp=True
+    polish=True
+    init='sobol'
+    atol=0.0000001
+    tol=0.00000001
+    s=differential_evolution(funcSt,strategy=strats,maxiter=maxIter,popsize=popSize,mutation=mutation,recombination=CR,disp=disp,polish=polish,bounds=boundsSS,init=init,atol=atol,tol=tol,updating='immediate',x0=guessSS)
+    print(s.x)
+    print(s.success)
+    print(s.message)
+    return s.x
+
+
+
 def SHFr():
     maxIter=4
     s=shgo(funcFr,bounds=boundsFr,iters=maxIter,options={'disp':True})
     print(s.x)
     print(s.success)
     print(s.message)
-    print(funcFr(s.x,True))  
-def dualFr():
-    maxIter=1000
-    s=dual_annealing(funcFr,bounds=boundsFr,maxiter=maxIter,x0=guessFr)
-    print(s.x)
-    print(s.success)
-    print(s.message)
     print(funcFr(s.x,True)) 
-def HoppingFr():
-    maxIter=1000
-    s=basinhopping(funcFr,guessFr)
+def SHFrSS():
+    maxIter=5
+    s=shgo(funcSt,bounds=boundsSS,iters=maxIter,options={'disp':True})
     print(s.x)
     print(s.success)
     print(s.message)
-    print(funcFr(s.x))
+    print(funcSt(s.x,True)) 
+
+
+
+
+
+
 
 if __name__ == '__main__':  
-    #evolve()
+    print(funcFr(guessFr,True))
+    #print(funcSt(guessSS,True))
     #evolveFr()
-    #print(func(guess,True))
-    #dualFr()
-    #SHFr()
-    funcFr(guessFr,True)
-    #SH()
-    #minimizationFr()
