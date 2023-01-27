9415c27b04c5cd3ea29f22516bbaa4ca689747aa
fdtroya <fdtroya@espol.edu.ec>
2023-01-27 17:11:37 +0900

    .

--- tests/protocol2_0/Custom/control/systemID/optimize.py
+++ tests/protocol2_0/Custom/control/systemID/optimize.py
@@ -179,7 +179,7 @@ def SHFrSS():
 if __name__ == '__main__':  
     #print(funcM(guessM,True))
     #print(funcFr(guessFr,True))
-    print(funcFr(guessFr,True))
+    #print(funcFr(guessFr,True))
     #print(funcSt(guessSS,True))
     #evolveFrSS()
-    #evolveFr()
+    evolveFr()
